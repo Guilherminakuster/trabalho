c82e866d95c72a83d9610c0bf12f70187bad8496
Guilherminakuster <160687121+Guilherminakuster@users.noreply.github.com>
2024-03-20 15:25:43 -0300

    semana 6.py

---
+++ guilhermina2.py
@@ -0,0 +1,16 @@
+print ("Eduardo")
+
+numero = 10
+nome =  "João"
+booleano = True
+
+ # ordem de precedencia = not, and e or
+numero_real = 10.0
+
+resposta = (2 < 3) or (4 == 5 and numero != 10)
+
+# modificar a ordem das operações lógicas utilizando parênteses
+
+resultado = (True or False) and False
+
+print(resultado)
