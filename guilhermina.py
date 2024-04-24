e8309d87ebf1f323c34d7640ff5a5f9f560ab049
Guilherminakuster <160687121+Guilherminakuster@users.noreply.github.com>
2024-04-24 14:57:58 -0300

    Função 1 python

---
+++ guilhermina.py
@@ -0,0 +1,8 @@
+def imprimir_informacoes(nome, idade, cidade, sep=' - ', end='\n'):
+    print(f'Nome: {nome}', end=sep)
+    print(f'Idade: {idade}', end=sep)
+    print(f'Cidade: {cidade}', end=end)
+
+
+imprimir_informacoes('João', 30, 'São Paulo')
+
